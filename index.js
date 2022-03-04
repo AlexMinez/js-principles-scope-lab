669e20794bbbd510d1d1a134020b9ed0b3413e3e
AlexMinez <alex_minihan@outlook.com>
2022-03-04 14:16:11 +1100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    window.customerName = 'BOB'
+    return customerName
+}
+function setBestCustomer(){
+    window.bestCustomer = 'not bob'
+    return bestCustomer
+}
+function overwriteBestCustomer(){
+    window.bestCustomer = 'maybe bob'
+    return bestCustomer
+}
+const leastFavoriteCustomer = 'hello'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'goodbye'
+    return leastFavoriteCustomer
+}
